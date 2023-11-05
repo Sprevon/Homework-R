6fdfd5feda7ecfede9b9ffb376193dad1d71ca9d
Sprevon <sevenuniversity@outlook.com>
2023-11-05 21:30:31 +0800

    fix:修改文件说明

--- homework/homework1/Q2.R
+++ homework/homework1/Q2.R
@@ -1,4 +1,4 @@
-#读取csv
+# 第一次作业第二题
 bloodData <- read.csv("D:\\Program\\DataSpell\\datafile\\R-Class\\homework\\homework1\\data\\blood.csv")
 model <- lm(HGB ~ Ca + Mg + Fe + Mn + Cu, data = bloodData)
 summary(model)
--- homework/homework1/Q3.R
+++ homework/homework1/Q3.R
@@ -1,3 +1,4 @@
+# 第一次作业第三题
 houseData <- read.csv("D:\\Program\\DataSpell\\datafile\\R-Class\\homework\\homework1\\data\\USA_Housing.csv")
 model <- lm(AvgPrice ~ AvgAreaIncome + AvgAreaHouseAge +
 AvgAreaNumberRooms + AvgAreaNumberofBedrooms + AreaPopulation,
